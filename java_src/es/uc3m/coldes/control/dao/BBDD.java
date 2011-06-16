4461f522a11c108466cb60eee2346b7c8bff76bc
jmblanco1986 <jmblanco1986@e0e895d4-82b6-b72e-0bd5-b1833dfd7862>
2011-06-16 22:47:07 +0000

    Initial Commit

---
+++ java_src/es/uc3m/coldes/control/dao/BBDD.java
@@ -0,0 +1,93 @@
+package es.uc3m.coldes.control.dao;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class BBDD {
+
+	static Logger logger = Logger.getLogger(BBDD.class.getName());
+
+	private String url;
+	private String port;
+	private String schema;
+	private String username;
+	private String password;
+
+	public BBDD() {
+		Properties colDesPropierties = new Properties();
+
+		try {
+			// Cargamos el archivo de propiedades de ColDes
+			logger.info("[BBDD]: Loading ColDes properties ...");
+			InputStream in = ClassLoader.getSystemResourceAsStream("coldes.properties");
+			colDesPropierties.load(in);
+			in.close();
+
+			// Leemos las propiedades relativas a la conexión con la BBDD
+			logger.info("[BBDD]: Reading propierties ...");
+			url = colDesPropierties.getProperty("db.url");
+			port = colDesPropierties.getProperty("db.port");
+			schema = colDesPropierties.getProperty("db.schema");
+			username = colDesPropierties.getProperty("db.username");
+			password = colDesPropierties.getProperty("db.password");
+
+			// Cargamos el driver JDBC
+			logger.info("Cargando conector MySQL");
+			Class.forName("com.mysql.jdbc.Driver");
+
+		} catch (ClassNotFoundException e) {
+			logger.error("[BBDD]: Error: ", e);
+		} catch (FileNotFoundException e) {
+			logger.error("[BBDD]: ColDes file properties not found: "
+					+ e.getLocalizedMessage());
+		} catch (IOException e) {
+			logger.error("[BBDD]: Error opening ColDes file properties: "
+					+ e.getLocalizedMessage());
+		}
+
+	}
+
+	public BBDD(Properties samProperties) {
+		try {
+			// Leemos las propiedades relativas a la conexión con la BBDD
+			logger.info("[BBDD]: Reading propierties ...");
+			url = samProperties.getProperty("db.url");
+			port = samProperties.getProperty("db.port");
+			schema = samProperties.getProperty("db.schema");
+			username = samProperties.getProperty("db.username");
+			password = samProperties.getProperty("db.password");
+
+			// Cargamos el driver JDBC
+			logger.info("[BBDD]: Opening MySQL connector...");
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			logger.error("[BBDD]: Error: ", e);
+		}
+
+	}
+
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			// Intentamos conectarnos a la base de Datos
+			logger.info("[BBDD]: Connecting to the database of ColDes");
+
+			conn = DriverManager.getConnection("jdbc:mysql://" + url + ":"
+					+ port + "/" + schema, username, password);
+
+			logger.info("[BBDD]: Database connection established");
+		} catch (SQLException ex) {
+			logger.error("[BBDD]: Error MySQL: ", ex);
+		}
+		return conn;
+
+	}
+
+}
